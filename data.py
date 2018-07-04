1c10bea8f7b8e2794384e9aa12541ab59e467980
Lawrence Wong <lcwong@mit.edu>
2018-07-04 04:58:34 -0400

    Coded upto candlestick.

--- data.py
+++ data.py
@@ -30,8 +30,50 @@ def company_daily(symbol):
              "symbol": symbol, 
              "apikey": api_key } 
     page = requests.get(url, params = data)
-    return pprint.pprint(page.json())
+    #pprint.pprint(page.json())
+    return page.json()
 
 
-company_daily("AAPL")
+
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+def json_to_dataframe(data):
+    '''Converts json to dataframe format'''
+    jdata = json.loads(json.dumps(data))
+    df = pd.DataFrame(jdata).transpose().astype(float)
+    df["Date"] = pd.to_datetime(df.index)
+    df.columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Date']
+
+    return df.set_index('Date')
+
+
+
+def simple_linechart(symbol, df):
+    '''produces line chart from given dataframe for symbol'''
+
+    ax = df.plot(y=df.columns[0:4], title ="Time Series Daily for " + symbol, figsize=(15, 9), legend=True, fontsize=12, grid=True)
+    ax.set_xlabel("Date", fontsize=12)
+    ax.set_ylabel("Dollars($)", fontsize=12)   
+    plt.show(block=False)
+
+
+
+from pandas_candlestick_ohlc import *
+
+def candlestick(df):
+    '''produces a candlestick of the given dataframe'''
+    pandas_candlestick_ohlc(df)
+
+
+def testcase():    
+    symbol = "AAPL"
+    data = company_daily(symbol)['Time Series (Daily)']
+    df = json_to_dataframe(data)
+    simple_linechart(symbol, df)
+    candlestick(df)
+
+testcase()
 
